04ce65f2fb0c531223110b003c309522a4e4a375
linhuarui1995 <764058804lhr@gmail.com>
2018-03-24 14:31:10 +0800

    asd

--- exportExamples/airline/Bug.java
+++ exportExamples/airline/Bug.java
@@ -58,9 +58,9 @@ public  class Bug implements Runnable{
     */
     public void run() {
 synchronized (this){         Num_Of_Seats_Sold++;                       // making the sale
-}        if (Num_Of_Seats_Sold > Maximum_Capacity)  // checking
+}synchronized (this){         if (Num_Of_Seats_Sold > Maximum_Capacity)  // checking
             StopSales = true;                      // updating
-    }
+}    }
 }
 
 
